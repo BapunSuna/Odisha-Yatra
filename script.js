dab3c55b8290d7d50fd2595214d5e73718cdb2b1
Bapun Suna <145285862+BapunSuna@users.noreply.github.com>
2024-01-26 11:50:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+/* <div class="icons">
+  <i class="bx bx-search"></i>
+  <i class="bx bx-user"></i>
+  <i class="bx bx-menu"></i>
+</div>; */
+
+
+// .icons{
+//     height: 4rem;
+//     }
+    
+//     #navigation-bar{
+//         display: flex;
+//         justify-content: space-between;
+//         background-image: url(sea\ beach.png);
+//         height: 100vh;
+//         background-repeat: no-repeat;
+//         background-size: cover;
+//     }
+    
+//     i{
+//         color:brown;
+//         margin-top: 4rem; 
+//         margin-right: 2.5rem;
+//         font-size:4.313rem;
+//         font-weight: 800;
+//     }
